c56ad18212f69adf7c6c2acf3555b917e1e72cfb
samusake <samusake@gmail.com>
2019-04-30 21:59:57 +0200

    Changes to be committed: modified: genSample.py

--- genSample.py
+++ genSample.py
@@ -7,42 +7,37 @@ Created on Mon Apr 29 13:09:10 2019
 """
 import numpy as np
 from scipy.special import factorial
+from scipy.misc import imrotate
+from scipy.interpolate import interp1d
+from scipy.stats import gaussian_kde
+
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import skimage as sk
+
 
 def wnm(n,m,xs):
     xs.astype(complex)
     if np.size(xs[0])>np.size(xs[1]):
         xs=xs.transpose()
-    if len(xs)==2:
-        qs=xs[0]
-        ps=xs[1]
-        Lag=np.array(1)
-        tmp=np.array(1)
-        qp2=np.power(qs,2)+np.power(ps,2)
-        qjp=qs-1j*ps
-        for cnt in range(n,0,-1): #n, n-1, .., 1.
-            tmp=tmp*(m-n+cnt)/(n+1-cnt)
-            Lag=tmp-2*qp2*Lag/cnt
-        ws=np.power(-1.,n)/np.pi*np.sqrt(factorial(n)/factorial(m)*np.power(2,m-n))*np.power(qjp,m-n)*(np.transpose(np.exp(-np.power(qs,2)))*np.exp(-np.power(ps,2)))*Lag
-    else:
-        #horner Method
-        [qs,ps]=np.meshgrid(xs,xs)
-        Lag=np.ones((len(xs),len(xs)),dtype=complex)
-        tmp=np.array(1)
-        qp2=np.power(qs,2)+np.power(ps,2)
-        qjp=qs-1j*ps
-        for cnt in range(n,0,-1): #n, n-1, .., 1.
-            tmp=tmp*(m-n+cnt)/(n+1-cnt)
-            Lag=tmp-2*qp2*Lag/cnt
-        p1=((-1.)**float(n))/np.pi
-        p2=np.sqrt(factorial(n)/factorial(m)*2**(m-n))
-        p3=np.power(qjp,(m-n))
-        p41=np.transpose(np.exp(-np.power([xs],2)))
-        p42=np.exp(-np.power([xs],2))
-        p4=p41*p42
+
+    #horner Method
+    [qs,ps]=np.meshgrid(xs,xs)
+    Lag=np.ones((len(xs),len(xs)),dtype=complex)
+    tmp=np.array(1)
+    qp2=np.power(qs,2)+np.power(ps,2)
+    qjp=qs-1j*ps
+    for cnt in range(n,0,-1): #n, n-1, .., 1.
+        tmp=tmp*(m-n+cnt)/(n+1-cnt)
+        Lag=tmp-2*qp2*Lag/cnt
+    p1=((-1.)**float(n))/np.pi
+    p2=np.sqrt(factorial(n)/factorial(m)*2**(m-n))
+    p3=np.power(qjp,m-n)
+    p41=np.transpose(np.exp(-np.power([xs],2)))
+    p42=np.exp(-np.power([xs],2))
+    p4=p41*p42
         
-        ws=p1 * p2 * p3 * p4 * Lag
+    ws=p1 * p2 * p3 * p4 * Lag
         
     if n==m:
         w0=1./np.pi*np.power(-1.,n)
@@ -52,10 +47,40 @@ def wnm(n,m,xs):
         for j in range(0,np.shape(ws)[0]):
             if np.isnan(ws[j,i]):
                 ws[j,i]=w0
-    return(ws)
+    return(np.real(ws))
 
-def sample()
+def probdist(w,phi):#w-wigner-function-matrix, phi-rotationangle
+    w2=sk.transform.rotate(w,phi,resize=False);
+    probdist=np.sum(w2,1);
+    probdist=probdist/np.sum(probdist)
+    return(probdist)
 
+def equispacedPoints(prob, xaxis, N ):
+    f=interp1d(xaxis,prob)
+    xaxis2=np.linspace(xaxis[0], xaxis[-1], N)
+    prob2=f(xaxis2)
+    return((xaxis2,prob2))
+    
+def cumulant(probd):
+    return(np.cumsum(probd))
+
+def sample(cum,xaxis,N):
+    dxd2=(xaxis[1]-xaxis[0])/2.
+    randn=np.random.random_sample(N)
+    f=interp1d(cum, xaxis)
+    randsample=f(randn)+dxd2
+    hist=np.histogram(randsample,  bins=np.linspace(xaxis[0]-dxd2, xaxis[-1]+dxd2, len(xaxis)+1))[0]
+    hist=hist/np.sum(hist)
+    return(hist)
+
+def prepareExpData(expdata, nDatapoints): 
+    #ToDo:
+    #use kerneldensity estimation to get probabillity distribution to get X equispaced Datapoints
+    return(0)
+
+
+
+    
 #%%
 
 fig=plt.figure()
@@ -71,12 +96,22 @@ w=wnm(0,0,lxs)+wnm(1,0,lxs)+wnm(0,1,lxs)+wnm(1,1,lxs)
 #w=wnm(1,1,lxs)
 ax.plot_surface(xs,ys,np.real(w), rstride=1, cstride=1, cmap='viridis', edgecolor='none')
 #%%
+probdx=probdist(w,0)
+#xaxis2, probdx2=equispacedPoints(probdx,lxs,30)
+plt.plot(lxs, probdx, xaxis2, probdx2)
 
 
+plt.show()
+#%%
+cumdx=cumulant(probdx)
+plt.plot(lxs, cumdx)
+plt.show()
 
-
-
-
+#%%
+histsample=sample(cumdx,lxs,10000)
+plt.plot(lxs,histsample,lxs,probdx)
+plt.show()
+#%%
 
 
 
