d635b6ebeb2dfc8ef62247f193735685bae8f5e5
herbert <herbert@wolpertinger.physik.uni-rostock.de>
2019-05-06 18:15:19 +0200

    modified:   __pycache__/genSample.cpython-35.pyc 	new file:   __pycache__/model.cpython-35.pyc 	modified:   main.py 	modified:   model.py

--- main.py
+++ main.py
@@ -17,15 +17,16 @@ import skimage as sk
 
 from numba import jit
 import time
+import pickle
 
 from genSample import *
-#from model import *
-
+from model import *
+#%%
 N=4 #dimension of rho
-s=4 #number of samples
-nphi=180 #number of angleSteps
+s=5000 #number of samples
+nphi=12 #number of angleSteps
 
-nxs=101
+nxs=21
 xmax=5
 lxs=np.linspace(-xmax, xmax, nxs)
 [xs, ys]=np.meshgrid(lxs,lxs);
@@ -46,5 +47,52 @@ ax.set_ylabel('phi')
 fig=plt.figure(2)
 ax=fig.add_subplot(111)
 ax.contourf(xs,ys,np.real(W[3]),levels=15)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+#%%
+stest=30
+Ptest, Wtest=generateDataset(N,stest,nphi,lxs)
+
+inputV=np.zeros((s,nxs*nphi))
+outputV=np.zeros((s,nxs*nxs))
+for i in range(0, len(P)):
+    inputV[i]=P[i].flatten()
+    outputV[i]=W[i].flatten()
+   
+testIn=np.zeros((stest,nxs*nphi))
+testOut=np.zeros((stest,nxs*nxs))
+for i in range(0,len(Ptest)):
+    testIn[i]=P[i].flatten()
+    testOut[i]=W[i].flatten()
+    
+#%%
+ai=simpleDeepNN(nxs,nphi)
+#%%
+ai.model.compile(optimizer=tf.train.AdamOptimizer(0.001),#tf.train.GradientDescentOptimizer(0.005),#optimizer=tf.train.AdamOptimizer(0.001),
+    loss='mean_squared_error')
+history=ai.model.fit(inputV, outputV, epochs=200, batch_size=32, verbose=1, validation_data=(testIn, testOut))
+
+#%%
+plt.semilogy(history.history['loss'])
+plt.semilogy(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+#%%
+
+fig=plt.figure(5)
+ax=fig.add_subplot(111)
+ax.contourf(xs,ys,np.real(Wtest[9]),levels=15)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+
+
+Wai=ai.model.predict(testIn)
+Wai=np.reshape(Wai,(30,nxs,nxs))
+fig=plt.figure(6)
+ax=fig.add_subplot(111)
+ax.contourf(xs,ys,Wai[9],levels=15)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
--- model.py
+++ model.py
@@ -4,5 +4,31 @@
 Created on Wed May  1 19:34:57 2019
 
 @author: herbert
-"""
 
+"""
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow import keras
+from keras.utils import to_categorical
+
+#%%
+class simpleDeepNN:
+    def __init__(self, nxs, nphi):
+        self.model=tf.keras.Sequential()
+        
+        self.model.add(layers.Dense(nphi*nxs, activation='linear'))
+        # Add another:
+        self.model.add(layers.Dense(1024, activation='linear'))
+        self.model.add(layers.Dense(1024, activation='linear'))
+        # Add a softmax layer with 10 output units:
+        self.model.add(layers.Dense(nxs*nxs, activation='linear'))
+        self.doneeps=0
+    def train(self, eps, inputV, outputV, verb=0):
+        self.model.compile(optimizer=tf.train.AdamOptimizer(0.001),
+              loss='mean_squared_error',
+              metrics=['accuracy', 'mean_squared_error'])
+        
+    def evaluate(self, testIn, testOut):
+        return(self.model.evaluate(testIn, testOut))
+    def predict(self, testIn):
+        return(self.model.predict(testIn))
