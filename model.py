4f52932d2a8b0ca03e24b85fc7a52b23c017062c
samusake <samusake@gmail.com>
2019-05-14 01:56:32 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed May  1 19:34:57 2019
 
-@author: herbert
-
-"""
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow import keras
