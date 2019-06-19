f5a44f8b72819bf40fd8ec47cb6948b5882e6cd3
samusake <herbert@wolpertinger.physik.uni-rostock.de>
2019-06-19 18:35:17 +0200

    modified:   WignerComplete/trainOnPlainData.py

--- WignerComplete/trainOnPlainData.py
+++ WignerComplete/trainOnPlainData.py
@@ -24,6 +24,7 @@ from tensorflow import keras
 from keras.utils import to_categorical
 from keras.models import model_from_json
 from tensorflow.keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import train_test_split
 
 import time
 from genSample import *
@@ -63,7 +64,7 @@ ai.model.compile(optimizer=keras.optimizers.RMSprop(0.001),#tf.train.GradientDes
 checkpoint = ModelCheckpoint('models/ai_checkpoint.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
-history=ai.model.fit(Dinput, outputV, epochs=10, batch_size=32, verbose=1, validation_split=0.1, callbacks=callbacks_list)
+history=ai.model.fit(Dinput, outputV, epochs=20, batch_size=32, verbose=1, validation_split=0.1, callbacks=callbacks_list)
 ai.model.count_params()
 #%%
 ai.model.load_weights('models/ai_checkpoint.h5')
@@ -86,32 +87,30 @@ plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 
 #%%
-'''
-Wai_orig=ai.model.predict(testIn)
+blub, Dtest, blub, Wtest= train_test_split(D, W, test_size=0.1)
+blub, Dtestin, blub, Wtestout= train_test_split(Dinput, outputV, test_size=0.1)
+blub=None
+
+Wai_orig=ai.model.predict(Dtestin)
 Wai=np.concatenate(Wai_orig)
-Wai=np.reshape(Wai, (30,nxs,nxs))
+Wai=np.reshape(Wai, (len(Wtest),nxs,nxs))
 
-fig, axs = plt.subplots(3, 6, sharex='col', sharey='row')
+fig, axs = plt.subplots(2, 6, sharex='col', sharey='row')
 
-contour=np.linspace(-0.5,1,50)
+#contour=np.linspace(-0.5,1,50)
 for i in range(0,6):
-    axs[0,i].contourf(px,py,Ptest[i],contour)
-    axs[0,i].set_ylabel('phi')
-    #axs[0,i].axis('equal')
-   
-    axs[1,i].contourf(xs,ys,Wtest[i],contour)
-    axs[1,i].set_ylabel('Y')
+    axs[0,i].contourf(xs,ys,Wtest[i],levels=15)
+    axs[0,i].set_ylabel('Y')
     #axs[1,i].axis('equal')
 
-    axs[2,i].contourf(xs,ys,Wai[i],contour)
-    axs[2,i].set_xlabel('X')
-    axs[2,i].set_ylabel('Y')
+    axs[1,i].contourf(xs,ys,Wai[i],levels=15)
+    axs[1,i].set_xlabel('X')
+    axs[1,i].set_ylabel('Y')
     #axs[2,i].axis('equal')
-axs[0,3].set_title('Probability distributions')
-axs[1,3].set_title('Theoretical wigner distribution')
-axs[2,3].set_title('AI prediction of wigner distribution')
+axs[0,3].set_title('Theoretical wigner distribution')
+axs[1,3].set_title('AI prediction of wigner distribution')
 
 plt.show()
-'''
+
 
 #%%
